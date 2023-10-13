367db75115be664927c550326495e96f95ebf2be
Connor MacNeil <50525187+WobblyRonnoc@users.noreply.github.com>
2023-10-13 17:14:55 +0000

    removed demo components

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,16 @@
-import { Component  } from 'react';
+import { Component, useState } from 'react';
 
-import Welcome from './components/Welcome';
-import Support from './components/Support';
 import ListCast from './components/ListCast';
 
 function App() {
-  const name = 'StarGazers'
   return (
     <div className="container">
       <article>
         <hgroup>
+          <h1>Meet the <i style={{ color: "SteelBlue" }}>StarGazers</i></h1>
           <img src="images/group.svg" alt="StarGazers Group" />
-          <Welcome name={name}/>
-          <p>Members of an <b>intergalactic alliance</b><br />
-            paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
+          <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
           <ListCast/>
-          <Support/>
         </hgroup>
       </article>
     </div>
--- src/components/Welcome.jsx
+++
@@ -1,4 +0,0 @@
-export default (props) => {
-  return  <h1>Meet the <i style={{ color: "SteelBlue" }}>{props.name}</i></h1>
-}
-
